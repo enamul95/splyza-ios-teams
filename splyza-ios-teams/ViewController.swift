92ac76ed9442a5450a76529cf8a8e0623678374a
Enamul Haque <ehaque95@gmail.com>
2022-09-04 09:42:47 +0600

    Initial Commit

---
+++ splyza-ios-teams/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  splyza-ios-teams
+//
+//  Created by Enamul Haque on 4/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
